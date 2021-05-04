9952887ed3981acf3b82ad160e59768981e0f779
Cody Kimb <codykimbell@gmail.com>
2021-05-04 14:52:57 -0500

    portfolio links updated

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,24 +5,82 @@ function Portfolio() {
   return (
     <section class="work-section" id="work">
         <div class="grid-container">
-            <a href= "https://nowplayingapp.herokuapp.com/" target="_blank" class="grid-item item-1">
-                <h4>Now Playing: Music App</h4>
-            </a>
-            <a href= "https://drewvena.github.io/git-it-right/" target="_blank" class="grid-item item-2">
-                <h4>Cinequiz: Test Your Knowledge</h4>
-            </a>
-            <a href= "https://tech-blog-by-cody.herokuapp.com/"  target="_blank" class="grid-item item-3">
-                <h4>Mock Tech Blog</h4>
-            </a>
-            <a href= "https://www.bluebonnet-records.com/" target="_blank" class="grid-item item-4">
-                <h4>Bluebonnet Records</h4>
-            </a>
-            <a href= "https://www.codykimb.com/" target="_blank" class="grid-item item-5">
-                <h4>Music / Production</h4>
-            </a>
-            <a href= "https://codykimb.github.io/work-day-scheduler/" target="_blank" class="grid-item item-6">
-                <h4>Work Day Scheduler</h4>
-            </a>
+            <div class="grid-item item-1">
+                <div class="grid title">
+                    <h4>Now Playing: Music App</h4>
+                    <h6>
+                        <a href= "https://nowplayingapp.herokuapp.com/" target="_blank">DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/drewvena/now-playing" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-2">
+                <div class="grid title">
+                    <h4>Cinequiz: Test Your Knowledge</h4>
+                    <h6>
+                        <a href= "https://drewvena.github.io/git-it-right/" target="_blank">DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/drewvena/git-it-right" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-3">
+                <div class="grid title">
+                    <h4>Mock Tech Blog</h4>
+                    <h6>
+                        <a href= "https://tech-blog-by-cody.herokuapp.com/"  target="_blank">DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/codykimb/MVC-tech-blog" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-4">
+                <div class="grid title">
+                    <h4>Photo Port</h4>
+                    <h6>
+                        <a href= "https://codykimb.github.io/photo-port/" target="_blank">DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/codykimb/photo-port" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-5">
+                <div class="grid title">
+                    <h4>Budget Tracker</h4>
+                    <h6>
+                        <a href= "https://damp-earth-27271.herokuapp.com/ (Links to an external site.)" target="_blank" >DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/codykimb/PWA-budget-tracker" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-6">
+                <div class="grid title">
+                    <h4>Work Day Scheduler</h4>
+                    <h6>
+                        <a href= "https://codykimb.github.io/work-day-scheduler/" target="_blank" >DEPLOYED</a>
+                        <span> | </span>
+                        <a href= "https://github.com/drewvena/now-playing" target="_blank">REPOSITORY</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-7">
+                <div class="grid title">
+                    <h4>Bluebonnet Records</h4>
+                    <h6>
+                        <a href= "https://www.bluebonnet-records.com/" target="_blank">DEPLOYED</a>
+                    </h6>
+                </div>
+            </div>
+            <div class="grid-item item-8">
+                <div class="grid title">
+                    <h4>Music / Production</h4>
+                    <h6>
+                        <a href= "https://www.codykimb.com/" target="_blank" >DEPLOYED</a>
+                    </h6>
+                </div>
+            </div>
             {/* <div class="grid-item item-7"></div> */}
         </div>
 
