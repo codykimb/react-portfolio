53b9afbaf2ce08aef51e1437fd4548074a8c41e3
Cody Kimb <codykimbell@gmail.com>
2021-05-04 18:19:06 -0500

    resume update and styles

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,6 +4,9 @@ function Resume() {
   return (
     <main>
         <section class="resume about-section">
+            <a href="/assets/docs/Coding Resume.pdf" download>
+              <h5 class="dl-link">Full Resume Download</h5>
+            </a>
             <h2 class="resume-item">Technical Skills</h2>
             <ul class="resume-item">
                 <li>Languages: JavaScript, jQuery, HTML, CSS, SQL
@@ -11,6 +14,24 @@ function Resume() {
                 <li>Applications: GitHub, MySQL, NoSQL
                 </li>
                 <li>Tools: MongoDB, Express.js, React, Node.js, Bootstrap, Handlebars
+                </li>
+            </ul>
+            <h2 class="resume-item">Education</h2>
+            <ul class="resume-item">
+                <li>Certificate in Full Stack Web Development from The University of Texas
+                </li>
+                <li>Certificate in Business Foundations from The University of Texas
+                </li>
+                <li>Bachelor of Arts in Music from The University of Texas
+                </li>
+            </ul>
+            <h2 class="resume-item">Experience</h2>
+            <ul class="resume-item">
+                <li>Bluebonnet Records - Owner <i>(2019-present)</i>
+                </li>
+                <li>Round Rock ISD - Music Teacher <i>(2017-2018)</i>
+                </li>
+                <li>ACL Live @ The Moody Theater - Box Office <i>(2014-2016)</i>
                 </li>
             </ul>
         </section>
